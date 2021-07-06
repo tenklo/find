5de8ff0dc692f0c19aa2494b423acf4ba1a04029
iDanielD <deibertdaniel99@gmail.com>
2021-07-06 08:54:11 +0200

    project structure again

---
+++ Applications/FindUpdater/FindUpdater/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
